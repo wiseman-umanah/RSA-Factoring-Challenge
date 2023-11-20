c4a6c8a3c5508683ee9dccfca0c2ffcb42c17af8
wiseman-umanah <wisemanumanah@gmail.com>
2023-11-20 21:46:34 +0100

    Updated code to improve runtime speed

--- factors.py
+++ factors.py
@@ -5,22 +5,18 @@ import sys
 def findFac(num):
 	num = int(num)
 	factor = 2
-	while num != 0:
-		if num % factor == 0:
-			return (int(num / factor), factor)
-		factor += 1
+	while num % factor:
+		if factor <= num:
+			factor += 1
+	return ((num // factor), factor)
 
 
-def main(filename):
-	factors = 0
-	with open(filename, "r") as fp:
-		for i in fp:
-			factors = findFac(i)
-			print(f"{int(i)}={factors[0]}*{factors[1]}")
+if len(sys.argv) != 2:
+	sys.exit("Usage: {0}: <filename>".format(sys.argv[0]))
 
-
-if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print("Usage: {0}: <filename>".format(sys.argv[0]))
-	else:
-		main(sys.argv[1])
+factors = 0
+filename = sys.argv[1]
+with open(filename, "r") as fp:
+	for i in fp:
+		factors = findFac(i)
+		print(f"{int(i)}={factors[0]}*{factors[1]}")
